c71c60d580800bc4b713aa0b70a9606f60f70bd8
Stian Haga <hagastian@gmail.com>
2023-12-21 13:03:19 +0100

    fix: add color to game page heading

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -5,7 +5,7 @@
 <div class="py-10">
 	<header>
 		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-			<h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Games</h1>
+			<h1 class="text-3xl font-bold leading-tight tracking-tigh text-white">Games</h1>
 		</div>
 	</header>
 	<main>
