d5b0f19d10a82f9334c8e06da8b471cfd2c42a49
Stian Haga <hagastian@gmail.com>
2023-12-25 21:18:08 +0100

    feat: add high score table design

--- src/routes/games/[code]/+page.svelte
+++ src/routes/games/[code]/+page.svelte
@@ -140,14 +140,47 @@
 					</div>
 				</div>
 			</form>
-
-			<ol class="mt-10">
-				{#each players as player (player.uuid)}
-					<li>{player.nickname} - {player.maxScore}</li>
-				{:else}
-					<li>No participants yet!</li>
-				{/each}
-			</ol>
+			<div class="mt-8 flow-root">
+				<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+					<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+						<table class="min-w-full divide-y divide-gray-700">
+							<thead>
+								<tr>
+									<th
+										scope="col"
+										class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
+									>
+										Rank
+									</th>
+									<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">
+										Name
+									</th>
+									<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">
+										Score
+									</th>
+								</tr>
+							</thead>
+							<tbody class="divide-y divide-gray-800">
+								{#each players as player, i (player.uuid)}
+									<tr>
+										<td
+											class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
+										>
+											#{i + 1}
+										</td>
+										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
+											>{player.nickname}</td
+										>
+										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
+											>{player.maxScore}</td
+										>
+									</tr>
+								{/each}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
 		</div>
 	</main>
 {/if}
