1d4f40e6e5b20a91a260bdad7202879d72db2e1f
Stian Haga <hagastian@gmail.com>
2023-12-26 10:03:49 +0100

    feat: add badge component

---
+++ src/lib/components/Badge.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let color: 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink' =
+		'green';
+	export let text = '';
+
+	const getColorClass = () => {
+		switch (color) {
+			case 'gray':
+				return 'bg-gray-50 text-gray-600 ring-gray-500/10';
+			case 'red':
+				return 'bg-red-50 text-red-700 ring-red-600/10';
+			case 'yellow':
+				return 'bg-yellow-50 text-yellow-800 ring-yellow-600/20';
+			case 'blue':
+				return 'bg-blue-50 text-blue-700 ring-blue-700/10';
+			case 'indigo':
+				return 'bg-indigo-50 text-indigo-700 ring-indigo-700/10';
+			case 'purple':
+				return 'bg-purple-50 text-purple-700 ring-purple-700/10';
+			case 'pink':
+				return 'bg-pink-50 text-pink-700 ring-pink-700/10';
+			case 'green':
+			default:
+				return 'bg-green-50 text-green-700 ring-green-600/20';
+		}
+	};
+</script>
+
+<span
+	class={`inline-flex items-center rounded-md px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset ${getColorClass()}`}
+	>{text}</span
+>
