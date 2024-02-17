5aa4130a56fd6a4ea3a28056cb780d53cdedf136
Stian Haga <hagastian@gmail.com>
2024-02-17 21:59:19 +0100

    feat: create GameHighScore component

--- src/routes/games/[code]/+page.svelte
+++ src/routes/games/[code]/+page.svelte
@@ -5,6 +5,7 @@
 	import { capitalize } from '$lib/utils/casing.js';
 	import { fade } from 'svelte/transition';
 	import { formatTimeDelta } from '$lib/utils/dateUtils.js';
+	import GameHighScore from './GameHighScore.svelte';
 
 	export let data;
 
@@ -51,10 +52,12 @@
 
 	const millisecondsToEnd = new Date(data.game.end_at).getTime();
 	let millisecondsNow = new Date().getTime();
-	let timeLeftText = formatTimeDelta(millisecondsToEnd - millisecondsNow);
+	let timeLeft = millisecondsToEnd - millisecondsNow;
+	let timeLeftText = timeLeft > 0 ? formatTimeDelta(timeLeft) : 'Game has ended';
+
 	const timer = setInterval(() => {
 		millisecondsNow = new Date().getTime();
-		const timeLeft = millisecondsToEnd - millisecondsNow;
+		timeLeft = millisecondsToEnd - millisecondsNow;
 		if (timeLeft <= 0) {
 			clearInterval(timer);
 			timeLeftText = 'Game has ended';
@@ -107,151 +110,104 @@
 	</header>
 	<main>
 		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 py-5 mt-5">
-			<p>
-				{isParticipating
-					? 'Register new 2FA Code'
-					: 'Choose your nickname and enter your 2FA code to join the game!'}
-			</p>
-			<form method="post" action="?/updateScore">
-				<input
-					type="hidden"
-					name="is-participating"
-					id="is-participating"
-					value={isParticipating}
-				/>
-				<input type="hidden" name="game-id" id="game-id" value={data.game.id} />
-				<div class="mt-4 grid gap-x-6 gap-y-8 grid-cols-3">
-					{#if !isParticipating}
-						<div class="col-span-3">
-							<label for="nickname" class="block text-sm font-medium leading-6 text-white"
-								>Nickname</label
+			{#if timeLeft > 0}
+				<form method="post" action="?/updateScore">
+					<p>
+						{isParticipating
+							? 'Register new 2FA Code'
+							: 'Choose your nickname and enter your 2FA code to join the game!'}
+					</p>
+					<input
+						type="hidden"
+						name="is-participating"
+						id="is-participating"
+						value={isParticipating}
+					/>
+					<input type="hidden" name="game-id" id="game-id" value={data.game.id} />
+					<div class="mt-4 grid gap-x-6 gap-y-8 grid-cols-3">
+						{#if !isParticipating}
+							<div class="col-span-3">
+								<label for="nickname" class="block text-sm font-medium leading-6 text-white"
+									>Nickname</label
+								>
+								<div class="mt-2">
+									<div
+										class="relative flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-clash-500"
+									>
+										<input
+											type="text"
+											name="nickname"
+											id="nickname"
+											autocomplete="name"
+											value={nickname}
+											required
+											minlength="3"
+											class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
+										/>
+										<button
+											class="absolute right-2 top-1/2 -translate-y-1/2"
+											type="button"
+											on:click={handleNicknameRefresh}
+										>
+											<span class="sr-only">Generate new nickname suggestion</span>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke-width="1.5"
+												stroke="currentColor"
+												class="w-6 h-6 origin-center [animation-duration:0.2s] [animation-iteration-count:1]"
+												class:animate-spin={recentRefresh}
+												aria-hidden="true"
+												focusable="false"
+											>
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+												/>
+											</svg>
+										</button>
+									</div>
+								</div>
+							</div>
+						{/if}
+						<div class="col-span-2">
+							<label for="2fa-score" class="block text-sm font-medium leading-6 text-white"
+								>2FA value</label
 							>
 							<div class="mt-2">
 								<div
-									class="relative flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-clash-500"
+									class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-clash-500"
 								>
 									<input
-										type="text"
-										name="nickname"
-										id="nickname"
+										type="number"
+										name="2fa-score"
+										id="2fa-score"
 										autocomplete="name"
-										value={nickname}
+										value={newScore}
 										required
-										minlength="3"
+										min="1"
+										max="99"
 										class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
 									/>
-									<button
-										class="absolute right-2 top-1/2 -translate-y-1/2"
-										type="button"
-										on:click={handleNicknameRefresh}
-									>
-										<span class="sr-only">Generate new nickname suggestion</span>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke-width="1.5"
-											stroke="currentColor"
-											class="w-6 h-6 origin-center [animation-duration:0.2s] [animation-iteration-count:1]"
-											class:animate-spin={recentRefresh}
-											aria-hidden="true"
-											focusable="false"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-											/>
-										</svg>
-									</button>
 								</div>
 							</div>
 						</div>
-					{/if}
-					<div class="col-span-2">
-						<label for="2fa-score" class="block text-sm font-medium leading-6 text-white"
-							>2FA value</label
-						>
-						<div class="mt-2">
-							<div
-								class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-clash-500"
+						<div class="col-span-1 self-end">
+							<button
+								type="submit"
+								class="w-full rounded-md bg-clash-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500"
 							>
-								<input
-									type="number"
-									name="2fa-score"
-									id="2fa-score"
-									autocomplete="name"
-									value={newScore}
-									required
-									min="1"
-									max="99"
-									class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
-								/>
-							</div>
+								{isParticipating ? 'Add Score' : 'Join Game'}
+							</button>
 						</div>
 					</div>
-					<div class="col-span-1 self-end">
-						<button
-							type="submit"
-							class="w-full rounded-md bg-clash-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500"
-						>
-							{isParticipating ? 'Add Score' : 'Join Game'}
-						</button>
-					</div>
-				</div>
-			</form>
-			<div class="mt-8 flow-root">
-				<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-					<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-						<table class="min-w-full divide-y divide-gray-700">
-							<thead>
-								<tr>
-									<th
-										scope="col"
-										class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
-									>
-										Rank
-									</th>
-									<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">
-										Name
-									</th>
-									<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">
-										Score
-									</th>
-								</tr>
-							</thead>
-							<tbody class="divide-y divide-gray-800">
-								{#each players as player, i (player.uuid)}
-									<tr>
-										<td
-											class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
-										>
-											#{i + 1}
-										</td>
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
-											>{player.nickname}</td
-										>
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"
-											>{player.maxScore}</td
-										>
-									</tr>
-								{:else}
-									<tr>
-										<td
-											class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
-										>
-											-
-										</td>
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">-</td>
-										<td class="whitespace">
-											<span class="text-sm text-gray-300">No scores yet</span>
-										</td>
-									</tr>{/each}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
+				</form>
+			{:else}
+				<p class="text-white">Final scores:</p>
+			{/if}
+			<GameHighScore {players} />
 		</div>
 	</main>
 {/if}
