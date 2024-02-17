a39f2752e1a2c3d8521a39e968529f7cf8d93475
Stian Haga <hagastian@gmail.com>
2024-02-17 22:45:10 +0100

    feat: add sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,15 @@
+import {sequence} from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+Sentry.init({
+    dsn: "https://e3619164ee187231b7ee10d1eabfed75@o4506763794972672.ingest.sentry.io/4506763796938752",
+    tracesSampleRate: 1
+})
+
+export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
 	const supabase = createSupabaseServerClient({
 		supabaseUrl: PUBLIC_SUPABASE_URL,
 		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
@@ -23,4 +30,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 			return name === 'content-range';
 		}
 	});
-};
+});
+export const handleError = Sentry.handleErrorWithSentry();
