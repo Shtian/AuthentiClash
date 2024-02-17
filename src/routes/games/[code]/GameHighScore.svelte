e00561fc532d8c7aeb1e94dd007de4234e772605
Stian Haga <hagastian@gmail.com>
2024-02-17 22:12:29 +0100

    feat: highlight current player

--- src/routes/games/[code]/GameHighScore.svelte
+++ src/routes/games/[code]/GameHighScore.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let players: { nickname: string; maxScore: number; uuid: string }[] = [];
+	export let currentPlayerId = '';
 </script>
 
 <div class="mt-8 flow-root">
@@ -24,8 +25,8 @@
 				</thead>
 				<tbody class="divide-y divide-gray-800">
 					{#each players as player, i (player.uuid)}
-						<tr>
-							<td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+						<tr class:bg-clash-800={player.uuid === currentPlayerId}>
+							<td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-2">
 								#{i + 1}
 							</td>
 							<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{player.nickname}</td>
@@ -33,7 +34,7 @@
 						</tr>
 					{:else}
 						<tr>
-							<td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+							<td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-2">
 								-
 							</td>
 							<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">-</td>
