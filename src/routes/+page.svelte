3e744568fcf3142cd27814a1b1b48c2a67e7eebe
Stian Haga <hagastian@gmail.com>
2024-02-17 22:30:04 +0100

    feat: use lucide icon components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { fly, scale } from 'svelte/transition';
 	import logo from '$lib/assets/authenticlash_logo.svg';
+	import { ArrowRightCircle } from 'lucide-svelte';
 	let visible = false;
 	onMount(() => {
 		visible = true;
@@ -23,20 +24,7 @@
 				class="mt-8 inline-flex gap-x-2 items-center self-center rounded-md bg-clash-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500"
 			>
 				Play Now
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-					/>
-				</svg>
+				<ArrowRightCircle class="w-6 h-6" />
 			</a>
 		</div>
 
--- src/routes/games/[code]/+page.svelte
+++ src/routes/games/[code]/+page.svelte
@@ -6,6 +6,7 @@
 	import { fade } from 'svelte/transition';
 	import { formatTimeDelta } from '$lib/utils/dateUtils.js';
 	import GameHighScore from './GameHighScore.svelte';
+	import { Copy, RefreshCw } from 'lucide-svelte';
 
 	export let data;
 
@@ -81,21 +82,7 @@
 					class="relative inline-flex gap-x-2 items-center self-center rounded-md bg-transparent px-2 py-2 text-sm font-semibold text-white hover:text-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500 transition-colors"
 					on:click={copyUrl}
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						class="lucide lucide-copy"
-						><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path
-							d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"
-						/></svg
-					>
+					<Copy />
 					{#if urlIsRecentlyCopied}
 						<span class="absolute -bottom-[0.875rem] text-sm text-white" in:fade out:fade
 							>Copied!</span
@@ -150,23 +137,11 @@
 											on:click={handleNicknameRefresh}
 										>
 											<span class="sr-only">Generate new nickname suggestion</span>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												stroke-width="1.5"
-												stroke="currentColor"
-												class="w-6 h-6 origin-center [animation-duration:0.2s] [animation-iteration-count:1]"
-												class:animate-spin={recentRefresh}
-												aria-hidden="true"
-												focusable="false"
-											>
-												<path
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-												/>
-											</svg>
+											<RefreshCw
+												class="w-6 h-6 origin-center [animation-duration:0.2s] [animation-iteration-count:1] {recentRefresh
+													? 'animate-spin'
+													: null}"
+											/>
 										</button>
 									</div>
 								</div>
