d318f016d5bb13782c0a984644ac7a60de8b5526
Stian Haga <hagastian@gmail.com>
2024-02-18 09:36:27 +0100

    fix(games): use rocket icon from lucide

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Badge from '$lib/components/Badge.svelte';
 	import { DateFormatter } from '@internationalized/date';
+	import { RocketIcon } from 'lucide-svelte';
 	export let data;
 	const df = new DateFormatter('en-US', {
 		dateStyle: 'medium',
@@ -115,20 +116,7 @@
 			</div>
 		{:else}
 			<div class="text-center">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="mx-auto h-12 w-12 text-gray-400"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-					/>
-				</svg>
+				<RocketIcon class="mx-auto h-12 w-12 text-gray-400" />
 				<h3 class="mt-2 text-sm font-semibold text-white">No active games</h3>
 				<p class="mt-1 text-sm text-white">Get started by creating a new AuthentiClash session.</p>
 				<div class="mt-6">
