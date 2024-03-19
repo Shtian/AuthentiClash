62d81c98e0e48535e0c5c6c6566c4bb31748eb56
Stian Haga <hagastian@gmail.com>
2024-03-19 15:48:52 +0100

    feat: add discord login

--- src/routes/auth/login/AuthProviders.svelte
+++ src/routes/auth/login/AuthProviders.svelte
@@ -2,17 +2,25 @@
 	import { toast } from '$lib/stores/ToastStore.js';
 	import type { SupabaseClient } from '@supabase/supabase-js';
 	import { Github } from 'lucide-svelte';
+	import discordLogo from '$lib/assets/discord_logo_white.svg';
 	export let supabase: SupabaseClient<any, 'public', any>;
 
-	const githubLogin = async () => {
-		const { data, error } = await supabase.auth.signInWithOAuth({ provider: 'github' });
+	type OAuthProvider = 'github' | 'discord';
+
+	const oAuthLogin = async (provider: OAuthProvider) => {
+		const { data, error } = await supabase.auth.signInWithOAuth({ provider });
 		if (error) {
-			toast.send({ message: `Error logging in with GitHub: ${error.message}`, type: 'error' });
+			toast.send({ message: `Error logging in with ${provider}: ${error.message}`, type: 'error' });
 		}
 	};
 </script>
 
 <button
 	class="flex w-full justify-center rounded-md bg-clash-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500"
-	on:click={() => githubLogin()}><Github class="size-6 mr-1" /> GitHub</button
+	on:click={() => oAuthLogin('github')}><Github class="size-6 mr-1" /> GitHub</button
+>
+<button
+	class="flex w-full justify-center rounded-md bg-clash-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500 mt-1"
+	on:click={() => oAuthLogin('discord')}
+	><img class="size-6 mr-1" width="24" height="24" src={discordLogo} alt="Discord logo" /> Discord</button
 >
