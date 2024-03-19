524ff82a93525ccf883c9063b5e4b81728f853e0
Stian Haga <hagastian@gmail.com>
2024-03-19 18:54:43 +0100

    feat: add google login

--- src/routes/auth/login/AuthProviders.svelte
+++ src/routes/auth/login/AuthProviders.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
 	import { toast } from '$lib/stores/ToastStore.js';
 	import type { SupabaseClient } from '@supabase/supabase-js';
-	import { Github } from 'lucide-svelte';
 	import discordLogo from '$lib/assets/discord_logo_white.svg';
+	import googleLogo from '$lib/assets/google_logo.svg';
+	import githubLogo from '$lib/assets/github_logo.svg';
+
 	export let supabase: SupabaseClient<any, 'public', any>;
 
-	type OAuthProvider = 'github' | 'discord';
+	type OAuthProvider = 'github' | 'discord' | 'google';
 
 	const oAuthLogin = async (provider: OAuthProvider) => {
-		const { data, error } = await supabase.auth.signInWithOAuth({ provider });
+		const options =
+			provider === 'google'
+				? {
+						queryParams: {
+							access_type: 'offline',
+							prompt: 'consent'
+						}
+					}
+				: {};
+
+		const { data, error } = await supabase.auth.signInWithOAuth({ provider, options });
 		if (error) {
 			toast.send({ message: `Error logging in with ${provider}: ${error.message}`, type: 'error' });
 		}
@@ -16,11 +28,35 @@
 </script>
 
 <button
-	class="flex w-full justify-center rounded-md bg-clash-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500"
-	on:click={() => oAuthLogin('github')}><Github class="size-6 mr-1" /> GitHub</button
+	class="flex w-full justify-center rounded-md bg-clash-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500 mt-1"
+	on:click={() => oAuthLogin('google')}
+	><img
+		class="size-6 mr-3"
+		width="24"
+		height="24"
+		src={googleLogo}
+		alt="Google logo"
+	/>Google</button
 >
 <button
-	class="flex w-full justify-center rounded-md bg-clash-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500 mt-1"
+	class="flex w-full justify-center rounded-md bg-clash-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500 mt-2"
+	on:click={() => oAuthLogin('github')}
+	><img
+		class="size-6 mr-3"
+		width="24"
+		height="24"
+		src={githubLogo}
+		alt="Github logo"
+	/>GitHub</button
+>
+<button
+	class="flex w-full justify-center rounded-md bg-clash-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-clash-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-500 mt-2"
 	on:click={() => oAuthLogin('discord')}
-	><img class="size-6 mr-1" width="24" height="24" src={discordLogo} alt="Discord logo" /> Discord</button
+	><img
+		class="size-6 mr-3"
+		width="24"
+		height="24"
+		src={discordLogo}
+		alt="Discord logo"
+	/>Discord</button
 >
