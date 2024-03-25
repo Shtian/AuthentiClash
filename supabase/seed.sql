ff472580b6a1daf4f603632719160ba4e1231b32
Stian Haga <hagastian@gmail.com>
2024-03-25 20:58:19 +0100

    feat: add intial user seed script

---
+++ supabase/seed.sql
@@ -0,0 +1,72 @@
+-- create test users
+INSERT INTO
+    auth.users (
+        instance_id,
+        id,
+        aud,
+        role,
+        email,
+        encrypted_password,
+        email_confirmed_at,
+        recovery_sent_at,
+        last_sign_in_at,
+        raw_app_meta_data,
+        raw_user_meta_data,
+        created_at,
+        updated_at,
+        confirmation_token,
+        email_change,
+        email_change_token_new,
+        recovery_token
+    ) (
+        select
+            '00000000-0000-0000-0000-000000000000',
+            uuid_generate_v4 (),
+            'authenticated',
+            'authenticated',
+            'user' || (ROW_NUMBER() OVER ()) || '@example.com',
+            crypt ('password123', gen_salt ('bf')),
+            current_timestamp,
+            current_timestamp,
+            current_timestamp,
+            '{"provider":"email","providers":["email"]}',
+            '{}',
+            current_timestamp,
+            current_timestamp,
+            '',
+            '',
+            '',
+            ''
+        FROM
+            generate_series(1, 10)
+    );
+
+-- test user email identities
+INSERT INTO
+    auth.identities (
+        provider_id,
+        user_id,
+        identity_data,
+        provider,
+        last_sign_in_at,
+        created_at,
+        updated_at
+    ) (
+        select
+            uuid_generate_v4 (),
+            id,
+            format('{"sub":"%s","email":"%s"}', id::text, email)::jsonb,
+            'email',
+            current_timestamp,
+            current_timestamp,
+            current_timestamp
+        from
+            auth.users
+    );
+
+-- Add profiles for test users
+UPDATE public.profiles
+SET username = 'user ' || SUBSTRING(id::text FROM 1 FOR 8),
+    avatar_url = 'https://api.dicebear.com/8.x/pixel-art/svg?seed=' || SUBSTRING(id::text FROM 1 FOR 8),
+    updated_at = current_timestamp
+WHERE id IN (SELECT id FROM auth.users);
