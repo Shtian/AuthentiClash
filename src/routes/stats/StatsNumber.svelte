afc7cbce77ba5cae9f5aed1b73d98ee9c1ab97cc
Stian Haga <hagastian@gmail.com>
2024-03-28 09:37:14 +0100

    feat: add statsnumber component

---
+++ src/routes/stats/StatsNumber.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { tweened } from 'svelte/motion';
+	export let value: number;
+	export let decimals = 0;
+	export let duration = 300;
+	const progress = tweened(0, { duration });
+	progress.set(100);
+	$: currentNum = ($progress * value) / 100;
+</script>
+
+<span class="tabular-nums">{currentNum.toFixed(decimals)}</span>
