9d685793f5fc9c9c428d331c4341ac91ccea0c5c
Stian Haga <hagastian@gmail.com>
2024-03-31 08:29:42 +0200

    fix: correct typo in SupabaseResponse type value

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient, type PostgrestError } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
 export type SupabaseResponse<T> =
-	| { type: 'sucess'; data: T; error: null }
+	| { type: 'success'; data: T; error: null }
 	| { type: 'error'; data: null; error: PostgrestError };
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
