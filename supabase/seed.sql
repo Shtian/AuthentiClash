894b562b4f87afaa702677cb126aa689dd2c917a
Stian Haga <hagastian@gmail.com>
2024-03-31 10:30:11 +0200

    fix: add more badge data to seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -106,15 +106,15 @@ INSERT INTO
     );
 
 -- Add badges
-INSERT INTO badges (id, created_at, name, description, enabled, secret, slug) VALUES
-(1, NOW(), 'First Blood', 'Granted after securing your initial victory, marking the beginning of your ascent in AuthentiClash.', FALSE, FALSE, 'first-blood'),
-(2, NOW(), 'High Five', 'Awarded for reaching the landmark of 5 wins, showcasing your burgeoning prowess.', FALSE, FALSE, 'high-five'),
-(3, NOW(), 'The Fellowship of the Win', 'Bestowed for achieving 10 wins, proving your leadership and camaraderie in the face of challenge.', FALSE, FALSE, 'fellowship-of-the-win'),
-(4, NOW(), '20/20 Victory Vision', 'Conferred upon reaching 20 wins, a testament to your clarity of vision and unmatched skill.', FALSE, FALSE, '20-20-victory-vision'),
-(5, NOW(), 'AuthentiChampion', 'The ultimate accolade for surpassing 50 wins, crowning you as the champion of AuthentiClash.', FALSE, FALSE, 'authentichampion'),
-(6, NOW(), 'Rite of Passage', 'Granted after your first encounter with defeat, marking a crucial rite of passage in AuthentiClash.', FALSE, FALSE, 'rite-of-passage'),
-(7, NOW(), 'Echoes From the Abyss', 'Awarded to those who''ve experienced the shadow of last place in a total of 5 games, honoring your journey and perseverance.', FALSE, FALSE, 'echoes-from-the-abyss'),
-(8, NOW(), 'What Are the Odds', 'Bestowed for the serendipitous entry of a 2FA code that matches the current time, celebrating a rare moment of cosmic alignment.', FALSE, FALSE, 'what-are-the-odds'),
-(9, NOW(), 'Luck''s Fickle Finger', 'For when fate hands you the number 1, a badge recognizing the whims of chance and your resilience.', FALSE, FALSE, 'lucks-fickle-finger'),
-(10, NOW(), 'Cloud 99', 'Securing the coveted 99, you float on Cloud 99, where your luck and timing elevate you to meme legend status in AuthentiClash.', FALSE, FALSE, 'cloud-99'),
-(11, NOW(), 'Nice', 'Awarded for achieving the culturally iconic total of 69, a nod to internet humor and your fun-filled journey in AuthentiClash.', FALSE, FALSE, 'nice');
+INSERT INTO badges (id, created_at, name, description, enabled, secret, slug, image) VALUES
+(1, NOW(), 'First Blood', 'Granted after securing your initial victory, marking the beginning of your ascent in AuthentiClash.', FALSE, FALSE, 'first-blood', NULL),
+(2, NOW(), 'High Five', 'Awarded for reaching the landmark of 5 wins, showcasing your burgeoning prowess.', FALSE, FALSE, 'high-five', NULL),
+(3, NOW(), 'The Fellowship of the Win', 'Bestowed for achieving 10 wins, proving your worth in the face of challenge.', FALSE, FALSE, 'fellowship-of-the-win', NULL),
+(4, NOW(), '20/20 Victory Vision', 'Conferred upon reaching 20 wins, a testament to your clarity of vision and unmatched skill.', FALSE, FALSE, '20-20-victory-vision', NULL),
+(5, NOW(), 'AuthentiChampion', 'The ultimate accolade for surpassing 50 wins, crowning you as a champion of AuthentiClash.', FALSE, FALSE, 'authentichampion', NULL),
+(6, NOW(), 'Rite of Passage', 'Granted after your first encounter with defeat, marking a crucial rite of passage in AuthentiClash.', FALSE, FALSE, 'rite-of-passage', NULL),
+(7, NOW(), 'Echoes From the Abyss', 'Awarded to those who''ve experienced the shadow of last place in a total of 5 games, honoring your journey and perseverance.', FALSE, FALSE, 'echoes-from-the-abyss', NULL),
+(8, NOW(), 'What Are the Odds', 'Bestowed for the serendipitous entry of a 2FA code that matches the current hour, minute or seconds, celebrating a rare moment of cosmic alignment.', TRUE, TRUE, 'what-are-the-odds', 'https://mzuhtoiuhpkczppcdwza.supabase.co/storage/v1/object/public/badges/what-are-the-odds.webp'),
+(9, NOW(), 'Luck''s Fickle Finger', 'For when fate hands you the number 1, a badge recognizing the whims of chance and your resilience.', TRUE, FALSE, 'lucks-fickle-finger', 'https://mzuhtoiuhpkczppcdwza.supabase.co/storage/v1/object/public/badges/lucks-fickle-finger.webp'),
+(10, NOW(), 'Cloud 99', 'Reserved for those touched by serendipity, this marks a moment of extraordinary luck, as elusive and uplifting as the rarest stratus in the highest heavens.', TRUE, FALSE, 'cloud-99', 'https://mzuhtoiuhpkczppcdwza.supabase.co/storage/v1/object/public/badges/cloud-99.webp'),
+(11, NOW(), 'Nice', '', TRUE, TRUE, 'nice', 'https://mzuhtoiuhpkczppcdwza.supabase.co/storage/v1/object/public/badges/nice.webp');
