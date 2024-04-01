f760baf63ce8d63570291ee543ef993c8c4bbb80
Stian Haga <hagastian@gmail.com>
2024-04-01 20:16:02 +0200

    feat: enable the rest of the badges

--- src/lib/badges/badgeSlugs.ts
+++ src/lib/badges/badgeSlugs.ts
@@ -1,1 +1,12 @@
-export type BadgeSlug = 'lucks-fickle-finger' | 'cloud-99' | 'nice' | 'what-are-the-odds';
+export type BadgeSlug =
+	| 'lucks-fickle-finger'
+	| 'cloud-99'
+	| 'nice'
+	| 'what-are-the-odds'
+	| 'first-blood'
+	| 'high-five'
+	| 'fellowship-of-the-win'
+	| '20-20-victory-vision'
+	| 'authentichampion'
+	| 'rite-of-passage'
+	| 'echoes-from-the-abyss';
