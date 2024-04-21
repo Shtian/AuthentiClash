4cea7c845910567712174b46f01f04b56bc68982
Stian Haga <hagastian@gmail.com>
2024-04-21 08:37:59 +0200

    fix: rename badge overview

--- src/routes/badges/+page.svelte
+++ src/routes/badges/+page.svelte
@@ -10,7 +10,7 @@
 <div class="mx-auto max-w-[1200px] px-6 lg:px-8 lg:py-10">
 	<div class="mx-auto mb-8 max-w-2xl lg:mx-0">
 		<div class="flex items-center justify-between">
-			<h1 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Badges</h1>
+			<h1 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Badge progress</h1>
 			<CircularProgressBar total={badges.length} count={badges.filter((b) => b.unlocked).length} />
 		</div>
 		<p class="mt-6 text-lg leading-8 text-white">
