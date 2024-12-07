082cb41028f19019d9f15af08d9c38d263a86f60
Stian Haga <hagastian@gmail.com>
2024-12-07 15:08:46 +0100

    feat: add get class data function

--- src/lib/supabase/classes.ts
+++ src/lib/supabase/classes.ts
@@ -15,7 +15,7 @@ type Ability = {
 export const getAllClasses = async (): Promise<SupabaseResponse<Array<Class>>> => {
 	const { data, error } = await supabaseServerClient
 		.from('classes')
-		.select('id,name,description,abilities(id,name,description,created_at)');
+		.select('id,name,description,abilities(id,name,description)');
 
 	if (error) {
 		console.error('Error getting all classes:', error.message);
@@ -24,4 +24,24 @@ export const getAllClasses = async (): Promise<SupabaseResponse<Array<Class>>> =
 	}
 	console.log(data);
 	return { type: 'success', data: data || [], error: null };
+};
+
+export const getClass = async (classId: number | null): Promise<SupabaseResponse<Class | null>> => {
+	if (classId == null) {
+		return { type: 'success', data: null, error: null };
+	}
+
+	const { data, error } = await supabaseServerClient
+		.from('classes')
+		.select('id,name,description,abilities(id,name,description)')
+		.eq('id', classId)
+		.single();
+
+	if (error) {
+		console.error('Error getting class:', error.message);
+		const r: SupabaseResponse<Class> = { type: 'error', data: null, error };
+		return r;
+	}
+
+	return { type: 'success', data: data || null, error: null };
 };
