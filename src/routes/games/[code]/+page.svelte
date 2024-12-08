4a33d28b66cc418087230b3c7f607856da899b51
Stian Haga <hagastian@gmail.com>
2024-12-08 18:54:48 +0100

    fix: hide number controls for 2fa input in chrome/ff

--- src/routes/games/[code]/+page.svelte
+++ src/routes/games/[code]/+page.svelte
@@ -248,4 +248,13 @@
 			--a: 1turn;
 		}
 	}
+
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+	input[type='number'] {
+		-moz-appearance: textfield;
+	}
 </style>
