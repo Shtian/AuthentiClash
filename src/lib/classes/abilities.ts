ecc68d9206a0c0d7cc2a0c5827e1da04aff9210a
Stian Haga <hagastian@gmail.com>
2024-12-27 22:46:55 +0100

    feat: add class name helpers

--- src/lib/classes/abilities.ts
+++ src/lib/classes/abilities.ts
@@ -3,3 +3,17 @@ export const ABILITIES = {
 	CRIMSON_REAP: 2,
 	INFERNAL_RAGE: 3
 } as const;
+
+export const CLASSES = {
+	THIEF: 1,
+	NECROMANCER: 2,
+	BARBARIAN: 3
+} as const;
+
+export const CLASSES_NAME: {
+	[index: number]: string;
+} = {
+	[CLASSES.THIEF]: 'Thief',
+	[CLASSES.NECROMANCER]: 'Necromancer',
+	[CLASSES.BARBARIAN]: 'Barbarian'
+};
