eeb9f1680893b1b7e727841be5abec39e2935321
Stian Haga <hagastian@gmail.com>
2024-12-30 15:22:43 +0100

    feat: introduce filter tabs in overview

--- src/routes/games/+page.server.ts
+++ src/routes/games/+page.server.ts
@@ -1,34 +1,79 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import {
+	getActiveGamesByUserId,
+	getAllGamesCreatedByUserId,
+	getEndedGamesByUserId,
+	type Game
+} from '$lib/supabase/games';
+import { DateFormatter } from '@internationalized/date';
 
-export const load: PageServerLoad = async ({ locals: { getSession, supabase } }) => {
+export const load: PageServerLoad = async ({ locals: { getSession }, url }) => {
 	const session = await getSession();
 	if (!session) {
 		redirect(303, '/auth/login');
 	}
-
-	const { data: games, error } = await supabase
-		.from('games')
-		.select(
-			'id, creator, code, end_at, name, participation ( profile_id, score, total_score, nickname )'
-		);
-
-	if (error) {
-		return fail(500, { message: error });
-	}
-
 	const userId = session.user.id;
 	if (!userId) return fail(401, { message: 'User not found' });
 
-	const participatingGames = (games || []).filter(
-		(game) =>
-			game.creator === userId ||
-			game.participation.some((participation) => participation.profile_id === userId)
-	);
+	const filter = url.searchParams.get('filter');
+	console.log('filter: ', filter);
+	const gamesByFilter = await getGamesByFilter(filter, userId);
+
+	if (gamesByFilter.type === 'error') {
+		return fail(500, { message: gamesByFilter.error.message });
+	}
+	const participatingGames = sortParticipationAndFindUserRank(gamesByFilter.data, userId);
 
 	return {
 		participatingGames,
 		profileId: userId,
 		title: 'Games'
 	};
+};
+
+const sortParticipationAndFindUserRank = (games: Game[], userId: string) => {
+	if (!games) return [];
+
+	const df = new DateFormatter('en-US', {
+		dateStyle: 'medium',
+		timeStyle: 'short',
+		hour12: false
+	});
+
+	return games
+		.map((game) => {
+			const sortedParticipations = game.participation?.toSorted((a, b) => {
+				const aScore = a.totalScore || 0;
+				const bScore = b.totalScore || 0;
+				return bScore - aScore;
+			});
+			const userRankIndex = sortedParticipations.findIndex((p) => p.profileId === userId);
+			const userRank = userRankIndex === -1 ? 0 : userRankIndex + 1;
+			const participation = game.participation.find((p) => p.profileId === userId);
+			const endAtDate = new Date(game.end_at);
+			return {
+				id: game.id.toString(),
+				name: game.name,
+				code: game.code,
+				totalScore: participation?.totalScore || 0,
+				userRank,
+				endAtString: df.format(endAtDate),
+				endAtDate,
+				timeToEnd: endAtDate.getTime() - Date.now()
+			};
+		})
+		.sort((a, b) => b.timeToEnd - a.timeToEnd);
+};
+
+const getGamesByFilter = async (filter: string | null, userId: string) => {
+	switch (filter) {
+		case 'ended':
+			return getEndedGamesByUserId(userId);
+		case 'created':
+			return getAllGamesCreatedByUserId(userId);
+		case 'active':
+		default:
+			return getActiveGamesByUserId(userId);
+	}
 };
--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,42 +1,30 @@
 <script lang="ts">
-	import { DateFormatter } from '@internationalized/date';
-	import { RocketIcon } from 'lucide-svelte';
 	import GameList from './GameList.svelte';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
 	export let data;
-	const df = new DateFormatter('en-US', {
-		dateStyle: 'medium',
-		timeStyle: 'short',
-		hour12: false
-	});
-	const games = data.participatingGames || [];
-	const gamesWithParticipation = games
-		.map((game) => {
-			const sortedParticipations = game.participation?.toSorted((a, b) => {
-				const aScore = a.total_score || [0];
-				const bScore = b.total_score || [0];
-				return bScore - aScore;
-			});
-			const userRankIndex = sortedParticipations.findIndex((p) => p.profile_id === data.profileId);
-			const userRank = userRankIndex === -1 ? 0 : userRankIndex + 1;
-			const participation = game.participation.find((p) => p.profile_id === data.profileId);
-			return {
-				id: game.id,
-				name: game.name,
-				code: game.code,
-				totalScore: participation?.total_score || 0,
-				userRank,
-				endAtString: df.format(new Date(game.end_at)),
-				endAtDate: new Date(game.end_at),
-				timeToEnd: new Date(game.end_at).getTime() - Date.now()
-			};
-		})
-		.sort((a, b) => b.timeToEnd - a.timeToEnd);
+
+	$: games = data.participatingGames || [];
+
+	function tabSelected(e: Event) {
+		const eventTarget: HTMLSelectElement = e.target as HTMLSelectElement;
+		const tabname = eventTarget.value;
+		let url = new URL($page.url);
+		const currentFilter = url.searchParams.get('filter');
+		if (currentFilter === null && tabname === '') return;
+		if (currentFilter !== null && tabname === '') goto(`/games`);
+		if (currentFilter !== tabname) {
+			url.searchParams.set('filter', tabname);
+			goto(url);
+		}
+	}
 </script>
 
 <main>
 	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 lg:py-10">
-		<div class="mx-auto mb-8 max-w-2xl lg:mx-0">
-			<div class="flex place-content-between items-center">
+		<div class="heading mx-auto mb-6 max-w-2xl lg:mx-0">
+			<div class="mb-6 flex place-content-between items-center">
 				<h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Games</h2>
 				<a
 					href="/games/create"
@@ -44,37 +32,66 @@
 					>New game</a
 				>
 			</div>
-			<p class="mt-6 text-lg leading-8 text-white">
-				All games you are or have been participating in.
-			</p>
-		</div>
-		{#if games.length > 0}
-			<GameList {gamesWithParticipation} />
-		{:else}
-			<div class="text-center">
-				<RocketIcon class="mx-auto h-12 w-12 text-gray-400" />
-				<h3 class="mt-2 text-sm font-semibold text-white">No active games</h3>
-				<p class="mt-1 text-sm text-white">Get started by creating a new AuthentiClash session.</p>
-				<div class="mt-6">
-					<a
-						href="/games/create"
-						type="button"
-						class="inline-flex items-center rounded-md bg-clash-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-clash-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-clash-600"
+			<div class="sm:hidden">
+				<label for="tabs" class="sr-only">Select a tab</label>
+				<select
+					id="tabs"
+					name="tabs"
+					class="block w-full rounded-md border-none bg-white/5 py-2 pl-3 pr-10 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm"
+					on:change={tabSelected}
+				>
+					<option selected={$page.url.searchParams.get('filter') === null} value="">Active</option>
+					<option selected={$page.url.searchParams.get('filter') === 'ended'} value="ended"
+						>Ended</option
 					>
-						<svg
-							class="-ml-0.5 mr-1.5 h-5 w-5"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							aria-hidden="true"
-						>
-							<path
-								d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
-							/>
-						</svg>
-						New AuthentiClash
-					</a>
-				</div>
+					<option selected={$page.url.searchParams.get('filter') === 'created'} value="created"
+						>Created</option
+					>
+				</select>
 			</div>
+			<div class="hidden sm:block">
+				<nav class="flex border-b border-white/10 py-4">
+					<ul
+						role="list"
+						class="flex min-w-full flex-none gap-x-6 px-2 text-sm font-semibold leading-6 text-gray-400"
+					>
+						<a
+							href="/games"
+							class={`transition-colors ${$page.url.searchParams.get('filter') === null ? 'text-clash-200' : 'hover:text-white'}`}
+							>Active</a
+						>
+						<a
+							href="/games?filter=ended"
+							class={`transition-colors ${$page.url.searchParams.get('filter') === 'ended' ? 'text-clash-200' : 'hover:text-white'}`}
+							>Ended</a
+						>
+						<a
+							href="/games?filter=created"
+							class={`transition-colors ${$page.url.searchParams.get('filter') === 'created' ? 'text-clash-200' : 'hover:text-white'}`}
+							>Created</a
+						>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		{#if games.length}
+			<GameList gamesWithParticipation={games} />
+		{:else}
+			<p class="mt-6 text-white">
+				{#if $page.url.searchParams.get('filter') === null}
+					No active games
+				{:else if $page.url.searchParams.get('filter') === 'ended'}
+					No ended games
+				{:else if $page.url.searchParams.get('filter') === 'created'}
+					You have not created any games
+				{/if}
+			</p>
 		{/if}
 	</div>
 </main>
+
+<style>
+	.heading {
+		view-transition-name: game-heading;
+	}
+</style>
