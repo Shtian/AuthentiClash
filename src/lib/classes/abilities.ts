55d36f8883aafa005c589dfe935556074ad2a4ca
Stian Haga <hagastian@gmail.com>
2024-12-31 09:43:04 +0100

    feat: add warden ability enums

--- src/lib/classes/abilities.ts
+++ src/lib/classes/abilities.ts
@@ -2,14 +2,16 @@ export const ABILITIES = {
 	CUTPURSE: 1,
 	CRIMSON_REAP: 2,
 	INFERNAL_RAGE: 3,
-	DIVINE_AEGIS: 4
+	DIVINE_AEGIS: 4,
+	PROTECTORS_OATH: 5
 } as const;
 
 export const CLASSES = {
 	THIEF: 1,
 	NECROMANCER: 2,
 	BARBARIAN: 3,
-	PALADIN: 4
+	PALADIN: 4,
+	WARDEN: 5
 } as const;
 
 export const CLASSES_NAME: {
@@ -18,7 +20,9 @@ export const CLASSES_NAME: {
 	[CLASSES.THIEF]: 'Thief',
 	[CLASSES.NECROMANCER]: 'Necromancer',
 	[CLASSES.BARBARIAN]: 'Barbarian',
-	[CLASSES.PALADIN]: 'Paladin'
+	[CLASSES.PALADIN]: 'Paladin',
+	[CLASSES.WARDEN]: 'Warden'
 };
 
 export const DIVINE_AEGIS_PERCENTAGE = 0.75;
+export const PROTECTORS_OATH_PERCENTAGE = 0.25;
