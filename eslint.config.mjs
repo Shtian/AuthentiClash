1afb9fed05a91bb1c02c4f4273e84286dbe7b998
Stian Haga <hagastian@gmail.com>
2025-01-09 08:35:43 +0100

    fix: remove compatibility mode from eslint flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,75 +1,80 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import parser from 'svelte-eslint-parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+//@ts-check
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import node from 'eslint-plugin-n';
+import stylistic from '@stylistic/eslint-plugin-js';
+import globals from 'globals';
+import svelteParser from 'svelte-eslint-parser';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
 	{
-		ignores: [
-			'**/.DS_Store',
-			'**/node_modules',
-			'build',
-			'.svelte-kit',
-			'.vercel',
-			'package',
-			'**/.env',
-			'**/.env.*',
-			'!**/.env.example',
-			'src/lib/components/ui',
-			'**/pnpm-lock.yaml',
-			'**/package-lock.json',
-			'**/yarn.lock'
-		]
+		ignores: ['.vercel', 'node_modules', 'supabase', '.svelte-kit', 'src/lib/components/ui']
 	},
-	...compat.extends(
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
 	{
-		plugins: {
-			'@typescript-eslint': typescriptEslint
-		},
-
+		files: ['**/*.svelte', '*.svelte'],
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			},
-
-			parser: tsParser,
-			ecmaVersion: 2020,
-			sourceType: 'module',
-
+			parser: svelteParser,
 			parserOptions: {
-				extraFileExtensions: ['.svelte']
+				parser: tsParser
 			}
 		}
 	},
 	{
-		files: ['**/*.svelte'],
-
+		plugins: {
+			n: node,
+			'@stylistic': stylistic
+		},
 		languageOptions: {
-			parser: parser,
-			ecmaVersion: 5,
-			sourceType: 'script',
-
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
+			globals: {
+				...globals.browser,
+				...globals.node
 			}
+		},
+		rules: {
+			'@stylistic/quote-props': ['error', 'as-needed'],
+			'@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+			'@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+			'@typescript-eslint/ban-ts-comment': 'off',
+			'@typescript-eslint/ban-types': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/explicit-member-accessibility': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/naming-convention': 'off',
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-inferrable-types': 'off',
+			'@typescript-eslint/no-object-literal-type-assertion': 'off',
+			'@typescript-eslint/no-this-alias': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ args: 'after-used', argsIgnorePattern: '^_' }
+			],
+			'@typescript-eslint/no-use-before-define': 'off',
+			'@typescript-eslint/prefer-interface': 'off',
+			'no-constant-condition': ['error', { checkLoops: false }],
+			'no-duplicate-imports': 'error',
+			'no-empty': ['error', { allowEmptyCatch: true }],
+			'no-inner-declarations': 'off',
+			'no-restricted-properties': [
+				'error',
+				{ object: 'test', property: 'only', message: 'Do not check in test.only tests.' }
+			],
+			'no-sparse-arrays': 'off',
+			'prefer-const': ['error', { destructuring: 'all' }],
+			'no-var': 'error',
+			'object-shorthand': ['error', 'always'],
+			'prefer-arrow-callback': 'error',
+			'svelte/no-inner-declarations': 'off',
+			'n/prefer-global/process': ['off'],
+			'n/prefer-node-protocol': 'error'
 		}
 	}
 ];
